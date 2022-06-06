529165e183ef406c1b682c31ebc177e599a7c42a
Ping Wu <pingwu@mail.ustc.edu.cn>
2022-06-06 17:26:19 +0800

    1. add infinite-loop example, just halt cpu Get from https://github.com/metebalci/baremetal-rpi/tree/master/01-infinite-loop
    Signed-off-by: Ping Wu <pingwu@mail.ustc.edu.cn>

---
+++ arm64_programming_practice/infinite-loop/WINEnv.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PATH=..\..\mingw64\bin;%PATH%
+set PATH=..\..\xpack-openocd-0.11.0-4\bin;%PATH%
+set PATH=..\..\xpack-windows-build-tools-4.3.0-1\bin;%PATH%
+set PATH=..\..\gcc-arm-11.2-2022.02-mingw-w64-i686-aarch64-none-linux-gnu\bin;%PATH%
+
+rem set
+
+cmd
