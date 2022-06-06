e2f7514e63ec97f122130c674d7e097efa95fd9a
Ping Wu <pingwu@mail.ustc.edu.cn>
2022-06-06 16:38:44 +0800

    add gcc-arm-11.2-2022.02-mingw-w64-i686-aarch64-none-linux-gnu compiler
    Signed-off-by: Ping Wu <pingwu@mail.ustc.edu.cn>

---
+++ gcc-arm-11.2-2022.02-mingw-w64-i686-aarch64-none-linux-gnu/lib/gcc/aarch64-none-linux-gnu/11.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/data/jenkins/workspace/GNU-toolchain/arm-11-3/src/gcc/configure --target=aarch64-none-linux-gnu --prefix= --with-sysroot=/aarch64-none-linux-gnu/libc --with-build-sysroot=/data/jenkins/workspace/GNU-toolchain/arm-11-3/build-mingw-aarch64-none-linux-gnu/install//aarch64-none-linux-gnu/libc --with-bugurl=https://bugs.linaro.org/ --enable-shared --disable-libatomic --without-cloog --without-isl --disable-libssp --disable-libgomp --disable-libmudflap --disable-libquadmath --enable-checking=yes --enable-languages=c --with-gmp=/data/jenkins/workspace/GNU-toolchain/arm-11-3/build-mingw-aarch64-none-linux-gnu/host-tools --with-mpfr=/data/jenkins/workspace/GNU-toolchain/arm-11-3/build-mingw-aarch64-none-linux-gnu/host-tools --with-mpc=/data/jenkins/workspace/GNU-toolchain/arm-11-3/build-mingw-aarch64-none-linux-gnu/host-tools --enable-fix-cortex-a53-843419 --with-libiconv-prefix=/data/jenkins/workspace/GNU-toolchain/arm-11-3/build-mingw-aarch64-none-linux-gnu/host-tools --with-pkgversion='GNU Toolchain for the Arm Architecture 11.2-2022.02 (arm-11.14)'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
